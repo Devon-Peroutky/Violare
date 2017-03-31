4fede9f2be02dc981c7475bd14648995e9554244
dperoutky <dperoutky@twilio.com>
2017-03-30 20:34:22 -0700

    yo

--- client/modules/App.js
+++ client/modules/App.js
@@ -7,19 +7,6 @@ const App = React.createClass({
     console.log(error);
   },
 
-  componentDidMount() {
-    console.log("WAITING");
-    console.log("RENDERING THIS BUTTON");
-    gapi.signin2.render('g-signin2', {
-      'scope': 'https://www.googleapis.com/auth/plus.login',
-      'width': 200,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': this.onSignIn
-    });
-  },
-
   render: function() {
     function onSignIn(googleUser) {
       console.log("HEYYYYY");
@@ -68,7 +55,6 @@ const App = React.createClass({
               </ul>
           </div>
         </footer>
-        <div className="g-signin2" data-onsuccess={this.onSignIn} />
 		  </div>
     )
   },
@@ -80,7 +66,44 @@ const App = React.createClass({
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }  
+  },
+  componentDidMount: function() {
+    console.log("WAITING");
+    console.log("RENDERING THIS BUTTON");
+    gapi.signin2.render('g-signin2', {
+      'scope': 'https://www.googleapis.com/auth/plus.login',
+      'width': 200,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'on-success': this.onSignIn
+    });
+
+    gapi.load('auth2', function() {
+      auth2 = gapi.auth2.getAuthInstance({
+        client_id: '1093353630310-uu92ln0r8hqrc51vtdsvir3o0of2r5qa.apps.googleusercontent.com',
+        fetch_basic_profile: false,
+        scope: 'profile'
+      });
+
+      // Sign the user in, and then retrieve their ID.
+      auth2.signIn().then(function() {
+        console.log(auth2.currentUser.get().getId());
+      });
+    });
+
+    /*
+    if (auth2.isSignedIn.get()) {
+      console.log("BITCH JUST SIGNED IN!");
+      var profile = auth2.currentUser.get().getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }*/
+  }
 });
 
 module.exports = App;
